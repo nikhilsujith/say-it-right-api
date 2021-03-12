0619bb50927b94eafacd185e39a25693cdfb494f
Nikhil Sujith <niks@nikhils-mbp.myfiosgateway.com>
2021-03-12 11:18:59 -0500

    Add new user service created, need to handle unique error

--- src/main/java/com/nikhilsujith/sayitrightapi/repository/UserRepository.java
+++ src/main/java/com/nikhilsujith/sayitrightapi/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.nikhilsujith.sayitrightapi.repository;
 
 import com.nikhilsujith.sayitrightapi.model.User;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
 
 
 
--- src/main/java/com/nikhilsujith/sayitrightapi/service/UserService.java
+++ src/main/java/com/nikhilsujith/sayitrightapi/service/UserService.java
@@ -2,13 +2,16 @@ package com.nikhilsujith.sayitrightapi.service;
 
 import com.nikhilsujith.sayitrightapi.model.Group;
 import com.nikhilsujith.sayitrightapi.model.User;
+import com.nikhilsujith.sayitrightapi.repository.GroupRepository;
 import com.nikhilsujith.sayitrightapi.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,27 +23,32 @@ public class UserService {
 
 //    Get Repository
     @Autowired
-    UserRepository repository;
+    UserRepository userRepository;
+
+    @Autowired
+    GroupRepository groupRepository;
 
 //    Get all user data
     public List<User> getAllUserData(){
-        System.out.println(repository.findAll());
-        return repository.findAll();
+        return userRepository.findAll();
     }
 
 //    Get user by UserId
-    public Optional<User> getUserById(String userId){
-        return repository.findById(userId);
+    public Optional<User> getUserById(ObjectId userId){
+        return userRepository.findById(userId);
     }
 
     /*--------------------------POST--------------------------------------------*/
-    public void addNewUser(User user, Group group){
-        repository.save(user);
+    public void addNewUser(User user){
+       userRepository.save(user);
+
+
 //        repository.save(group);
 //        TODO
 //            Need to save group reference
 //                Help At:
 //            https://stackoverflow.com/questions/51943411/cannot-create-a-reference-to-an-object-with-a-null-id-mongo-hibernate-mongo-and
+//              https://stackoverflow.com/questions/43757776/save-document-with-dbref-in-mongodb-spring-data
     }
 
 }
